8765d674132edc3731bb2e8010b231159fb1e378
Nick Muchow <nickplusdrums@gmail.com>
2026-03-10 17:00:10 -0700

    Busy day at work!

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_directory_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_directory_abs, file_path))
+        valid_file_path = os.path.commonpath([working_directory_abs, target_file]) == working_directory_abs
+        if not valid_file_path:
+            return f"Error: Cannot read \"{file_path}\" as it is outside the working directory"
+        if not os.path.isfile(target_file):
+            return f"Error: File not found or is not a regular file: \"{file_path}\""
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content_string += f"[...File \"{file_path}\" truncated at {MAX_CHARS}] characters]"
+        return file_content_string
+    except:
+        return f"Error: Unable to execute get_file_content"
+
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,7 +6,7 @@ def get_files_info(working_directory, directory="."):
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
     if not valid_target_dir:
         return f"Error: Cannot list \"{directory}\" as it is outside the permitted working directory"
-    if not os.path.isdir(directory):
+    if not os.path.isdir(target_dir):
         return f"Error: \"{directory}\" is not a directory"
     data_list = []
     for item in os.listdir(target_dir):
